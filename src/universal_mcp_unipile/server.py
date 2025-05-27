b1079376b40799c615ae4e050b793432855961a2
Ankit Ranjan <arstejas7@gmail.com>
2025-05-28 00:50:41 +0530

    feat: Initial commit from MCP CLI init command

---
+++ src/universal_mcp_unipile/server.py
@@ -0,0 +1,19 @@
+
+from universal_mcp.servers import SingleMCPServer
+from universal_mcp.integrations import AgentRIntegration
+from universal_mcp.stores import EnvironmentStore
+
+from universal_mcp_unipile.app import UnipileApp
+
+env_store = EnvironmentStore()
+integration_instance = AgentRIntegration(name="unipile", store=env_store)
+app_instance = UnipileApp(integration=integration_instance)
+
+mcp = SingleMCPServer(
+    app_instance=app_instance,
+)
+
+if __name__ == "__main__":
+    mcp.run()
+
+
